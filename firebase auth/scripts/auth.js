73cec69ec30061bb6a7fbb47b8ddc73fe5cf2de2
TheHackerOne <yash2200@gmail.com>
2020-06-18 11:21:36 +0530

    updated firebase auth lessons

--- firebase auth/scripts/auth.js
+++ firebase auth/scripts/auth.js
@@ -1,5 +1,40 @@
 const signupForm =document.querySelector('#signup-form');
 
+//listening for auth status changes
+auth.onAuthStateChanged(user => {
+    if(user){
+        db.collection('guides').onSnapshot(snapshot => {
+            setupGuides(snapshot.docs);
+            setupUI(user);
+        }, (err) => console.log(err.message))
+    } else {
+        setupGuides([]);
+        setupUI(user);
+    }
+})
+
+// create new guide
+const createForm = document.querySelector('#create-form');
+createForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    db.collection('guides').add({
+        title: createForm['title'].value,
+        content: createForm['content'].value
+    })
+    .then(() => {
+        //close modal and reset form
+        const modal = document.querySelector('#modal-create');
+        M.Modal.getInstance(modal).close();
+        createForm.reset(); 
+    })
+    .catch(err => {
+        console.log(err.message)
+    })
+})
+
+
+//signup
 signupForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
@@ -10,18 +45,43 @@ signupForm.addEventListener('submit', (e) => {
 
     //signup user
     auth.createUserWithEmailAndPassword(email, password).then(cred => {
-        console.log(cred.user)
+        return db.collection('users').doc(cred.user.uid).set({
+            bio: signupForm['signup-bio'].value
+        })
+    }).then(() => {
         const modal = document.querySelector('#modal-signup');
         M.Modal.getInstance(modal).close();
         signupForm.reset(); 
     });
 })
 
+
+//logout
 const logout = document.querySelector('#logout');
 
 logout.addEventListener('click', (e) => {
     e.preventDefault();
-    auth.signOut().then(() => {
-        console.log('user logged out')
+    auth.signOut();
+    // auth.signOut().then(() => {
+    //     console.log('user logged out')
+    // })
+})
+
+
+//login
+const loginForm = document.querySelector('#login-form');
+
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const email = loginForm['login-email'].value;
+    const password = loginForm['login-password'].value;
+
+    auth.signInWithEmailAndPassword(email, password).then((cred) => {
+
+        //close the login modal and reset the form
+        const modal = document.querySelector('#modal-login');
+        M.Modal.getInstance(modal).close();
+        loginForm.reset(); 
     })
 })
